eccf4cf66c46700a54fd13de69847d947577fac9
Kur0den <git@kur0den.net>
2026-06-29 23:45:19 +0900

    色々と追加
    コミット忘れがち

--- go.mod
+++ go.mod
@@ -2,4 +2,15 @@ module github.com/kur0den/anytype_tagger
 
 go 1.26.2
 
-require github.com/epheo/anytype-go v0.53.1
+require (
+	github.com/epheo/anytype-go v0.53.1
+	github.com/openai/openai-go v1.12.0
+	github.com/openai/openai-go/v3 v3.41.0
+)
+
+require (
+	github.com/tidwall/gjson v1.18.0 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.1 // indirect
+	github.com/tidwall/sjson v1.2.5 // indirect
+)
--- main.go
+++ main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/epheo/anytype-go"
 	_ "github.com/epheo/anytype-go/client"
 
 	"github.com/openai/openai-go/v3"
+	"github.com/openai/openai-go/v3/option"
 )
 
 
@@ -25,14 +28,52 @@ type Config struct {
 
 func main() {
 	// configを取得
-	config, err := getConfigPath()
+	_, err := getConfigPath()
 	
 	if err != nil {
-		fmt.Println(err)
-		return
+		log.Fatalf("configPathがしゅとくできませんでした: %v", err)
 	}
-	fmt.Println(config)
-	// anytypeAuth()
+
+	config, err := getConfig()
+	
+	if err != nil {
+		log.Fatalf("configがしゅとくできませんでした: %v", err)
+	}
+
+	// client, err := anytypeAuth()
+	// if err != nil {
+	//   log.Fatalf("にんしょうがうまくいかなかったよ: %v", err)
+	// }
+
+	url, err := getAnytypeObjUrl()
+	if err != nil {
+		log.Fatalf("urlがしゅとくできませんでした: %v", err)
+	}
+	fmt.Println(url)
+
+
+	client := openai.NewClient(
+		option.WithAPIKey(config.LLM.Key),
+		option.WithBaseURL(config.LLM.Endpoint),
+	)
+
+	models, err := client.Models.List(context.Background())
+
+	if err != nil {
+		fmt.Println("modelsがしゅとくできませんでした")
+		panic(err)
+	}
+
+	modelId := ""
+	for _, model := range models.Data {
+		fmt.Println("Model:", model.ID)
+		modelId = model.ID
+	}
+
+	resp, err := getLLMResponse(modelId)
+
+
+	fmt.Println(resp.Choices[0].Message.Content)
 }
 
 func getConfigPath() (dir string, err error) {
@@ -68,13 +109,14 @@ func getConfig() (Config, error) {
 	config.LLM.Host = "192.168.0.20"
 	config.LLM.Port = "8080"
 	config.LLM.IsHttps = false
-	config.LLM.Endpoint = "http://192.168.0.20:8080/v1"
-
+	config.LLM.Endpoint = "http://192.168.0.20:8081/v1"
+	config.LLM.Key = "DUMMY"
 	return config, nil
+
 }
 
 
-func anytypeAuth() {
+func anytypeAuth() (*anytype.Client, error){
 	// clientを定義
 	client := anytype.NewClient(
 		anytype.WithBaseURL("http://localhost:31009"),
@@ -91,14 +133,46 @@ func anytypeAuth() {
 	// 入力されたコードで問い合わせ
 	token, err := client.Auth().CreateApiKey(ctx, auth.ChallengeID, code)
 	
-	fmt.Println(token)
-	fmt.Println(err)
-	
+	if err != nil {
+		return nil, fmt.Errorf("にんしょうのちゃれんじにしっぱいしたかも: %w", err)
+	}
+
 	// 取得したAPIキーを元にclientを再定義
 	client = anytype.NewClient(
 		anytype.WithBaseURL("http://localhost:31009"),
 		anytype.WithAppKey(token.ApiKey),
 	)
 
-	fmt.Println(client)
+	return client, nil
+}
+
+func readStdio(prompt) (string, err){
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print(prompt)
+	text, err := reader.ReadString('\n')
+	if err != nil {
+		return nil, fmt.Errorf("stdinのよみとりができなかったよ")
+	}
+	return text, nil
+}
+
+
+func getAnytypeObjUrl() (string, error) {
+	inputUrl, err = readStdio("タグ付けをしたいAnytypeのObjのディープリンクをいれてね")
+	
+}
+
+func getLLMResponse(modelId) {
+	resp, err := client.Chat.Completions.New(context.TODO(), openai.ChatCompletionNewParams{
+		Model: modelId,
+		Messages: []openai.ChatCompletionMessageParamUnion{
+			openai.DeveloperMessage("あなたは可愛い幼女なAIアシスタントです"),
+			openai.UserMessage("こんにちは"),
+		},
+	})
+
+	if err != nil {
+		fmt.Println("問い合わせに失敗したかも")
+		panic(err)
+	}
 }
